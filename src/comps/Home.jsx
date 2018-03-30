e7aa8eb3bb9eef3f21d702039d5d21bc30dbbb1c
Abigail Thelin <abbyjthelin@gmail.com>
2018-03-30 13:34:13 -0600

    router set up

---
+++ src/comps/Home.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class Home extends Component{
+
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                this is the home page
+            </div>
+        )
+    }
+}
+
+export default Home;
