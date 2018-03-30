25b2d146bfc5792ea6ec04fe72d92edc6b99492f
Abigail Thelin <abbyjthelin@gmail.com>
2018-03-30 13:49:08 -0600

    set up redux

---
+++ src/ducks/reducer.jsx
@@ -0,0 +1,39 @@
+const initialState = {
+    cart: []
+}
+
+//actions
+
+const ADD_TO_CART = "ADD_TO_CART";
+const REMOVE_ITEM= "REMOVE_ITEM";
+
+export function addToCart(id){
+    return{
+        type: ADD_TO_CART,
+        payload: id
+    }
+}
+
+export function removeItem(id){
+    return{
+        type: REMOVE_ITEM,
+        payload: id
+    }
+}
+
+//reducers
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        case: ADD_TO_CART:
+            if(){
+
+            }
+            break;
+        case: REMOVE_ITEM:
+            if(){
+
+            }
+            break;
+    }
+}
